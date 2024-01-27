fee4a9ef356b9fddd3525738da35d813e2b244bf
Ajay-dhisone <ajdhisone@gmail.com>
2024-01-27 15:37:21 +0000

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-const app = require("../server.js")
-module.exports = app
-// app.listen(process.env.LOCAL_HOST,()=>{console.log('listening : '+ process.env.LOCAL_HOST);})
+// const app = require("../server.js")
+// module.exports = app
+// // app.listen(process.env.LOCAL_HOST,()=>{console.log('listening : '+ process.env.LOCAL_HOST);})
+
+// /api/index.js 
+
+const app = require("../server.js"); 
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-const express = require("express")
-const app = express()
-const ejs = require("ejs")
-// const dotenv = require("dotenv")
-// dotenv.config();
+// const express = require("express")
+// const app = express()
+// const ejs = require("ejs")
+// // const dotenv = require("dotenv")
+// // dotenv.config();
 
-// app.set('view engine', 'ejs');
-// app.set('views', "public");
-// app.use(express.static("public"));
+// // app.set('view engine', 'ejs');
+// // app.set('views', "public");
+// // app.use(express.static("public"));
 
-app.get("/",(req,res)=>{
+// app.get("/",(req,res)=>{
 
-    res.render("index.ejs")
+//     res.render("index.ejs")
 
-})
+// })
 
+// module.exports = app;
+
+// server.js 
+
+const express = require("express"); 
+const app = express(); 
+
+// A simple get greet method 
+app.get("/greet", (req, res) => { 
+	// get the passed query 
+	const { name } = req.query; 
+	res.send({ msg: `Welcome ${name}!` }); 
+}); 
+
+// export the app for vercel serverless functions 
 module.exports = app;
