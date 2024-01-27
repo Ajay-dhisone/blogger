14ad2dcfdcec366f1101877b9948ee1a0a3bbf00
Ajay-dhisone <ajdhisone@gmail.com>
2024-01-27 15:27:02 +0000

    vercel hos

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
 const ejs = require("ejs")
-const dotenv = require("dotenv")
-dotenv.config();
+// const dotenv = require("dotenv")
+// dotenv.config();
 
-app.set('view engine', 'ejs');
-app.set('views', "public");
-app.use(express.static("public"));
+// app.set('view engine', 'ejs');
+// app.set('views', "public");
+// app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
 
